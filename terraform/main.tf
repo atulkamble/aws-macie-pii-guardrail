981c81e0666d1aa3bf45e3c1d9850094506d2bc7
Atul Kamble <atul_kamble@hotmail.com>
2025-09-02 10:29:22 +0530

    code

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,8 @@
 terraform {
-  required_version = ">= 1.6.0"
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = ">= 5.50.0"
+      source = "hashicorp/aws"
+      version = "6.11.0"
     }
   }
 }
